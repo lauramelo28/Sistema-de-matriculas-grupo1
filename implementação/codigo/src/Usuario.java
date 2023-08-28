3ab2232aefa5e509703e502edd08653822471a0c
Laura Melo <103376249+lauramelo28@users.noreply.github.com>
2023-08-28 20:26:39 -0300

    classe usuario

--- implementação/codigo/src/Usuario.java
+++ implementação/codigo/src/Usuario.java
@@ -1,19 +1,58 @@
 public abstract class Usuario {
     //#region ATRIBUTOS
-    private String nome;
-    private String cpf;
-    private String dataNascimento;
-    private String login;
-    private String senha;
+    protected String nome;
+    protected String cpf;
+    protected String dataNascimento;
+    protected String login;
+    protected String senha;
+    private Usuario user;
+
+
     //#endregion
 
     //#region MÉTODOS
-    public void realizarLogin(String login, String senha){
-        //Implementação do método
+
+    public Usuario(String nome, String cpf, String dataNascimento, String login, String senha){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.login = login; 
+        this.senha = senha;
     }
 
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public String getDataNascimento(){
+        return this.dataNascimento;
+    }
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+    
+    /*
+    public void realizarLogin(String login, String senha){
+        Usuario usuarioAtual = user.get(login);
+        
+        if(user !=null && usuarioAtual.getSenha().equals(senha)){
+            this.usuarioAtual = aluno;
+        } else{
+            this.usuarioAtual = null;
+        }
+
+    }*/
+
     public void realizarLogoff(){
-        //Implementação do método
+      //  this.usuarioAtual = null;
     }
     //#endregion
 }
