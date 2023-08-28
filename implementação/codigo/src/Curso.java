13087870d1c9d31be337c0a29cc62b6923bb8fb5
henriquegrissi <hgcsc2003@gmail.com>
2023-08-28 20:22:59 -0300

    feat(code): Curso and Discplinas added

--- implementação/codigo/src/Curso.java
+++ implementação/codigo/src/Curso.java
@@ -5,16 +5,56 @@ public class Curso {
     private String nome;
     private int numeroDeCreditos;
     private List<Disciplina> disciplinas;
+    private List<Aluno> alunos;
     //#endregion
 
+    //#region CONSTRUTORES
+    public Curso(String nome, int numeroDeCreditos, List<Disciplina> disciplinas) {
+        this.nome = nome;
+        this.numeroDeCreditos = numeroDeCreditos;
+        this.disciplinas = disciplinas;
+    }
     //#region MÉTODOS
-    public List<Disciplina> listarDisciplinas() {
-        //Implementação do método
-        return null;
+    public String getNome() {
+        return nome;
     }
 
+    /**
+     * Método para listar as disciplinas do curso
+     * @return String com as disciplinas do curso
+     */
+    public String listarDisciplinas() {
+        String disciplinasListada = "";
+        for(Disciplina disciplina : disciplinas){
+            disciplinasListada += disciplina.getNome() + "\n" + disciplina.getNumeroDeCreditos() + "\n";
+        }
+        return disciplinasListada;
+    }
+
+    /**
+     * Método para incluir uma disciplina na lista de disciplinas do curso
+     * @param disciplina
+     */
     public void adicionarDisciplinas(Disciplina disciplina){
-        //Implementação do método
+        this.disciplinas.add(disciplina);
+    }
+
+    /**
+     * Método para listar os alunos do curso
+     * @return String com os alunos do curso
+     */
+    public String listarAlunos() {
+        String alunosListados = "";
+        for(Aluno aluno : alunos){
+            alunosListados += aluno.getNome() + "\n" + aluno.getMatricula() + "\n";
+        }
+        return alunosListados;
+    }
+    /**
+     * Método para incluir um aluno na lista de alunos do curso
+     */
+    public void adicionarAluno(Aluno aluno){
+        this.alunos.add(aluno);
     }
     //#endregion
 }
--- implementação/codigo/src/Disciplina.java
+++ implementação/codigo/src/Disciplina.java
@@ -4,40 +4,64 @@ public class Disciplina {
     //#region ATRIBUTOS
     private String nome;
     private boolean ativa;
-    private List<Aluno> alunosMatriculadas;
-    private boolean periodoDeInscricaoLiberado;
+    private List<Aluno> alunosMatriculados;
+    private Professor professor;
+    private boolean periodoDeInscricaoLiberado; 
     private Curso curso;
-
+    private int numeroDeCreditos;
     private static final int MIN_DE_ALUNOS = 3;
     private static final int MAX_DE_ALUNOS = 60;
     //#endregion
 
+    //#region CONSTRUTORES
+    public Disciplina(String nome, int numeroDeCreditos, Curso curso){
+        this.nome = nome;
+        this.numeroDeCreditos = numeroDeCreditos;
+        this.curso = curso;
+    }
     //#region MÉTODOS
-    public void adicionarAluno(Aluno aluno){
-        //Implementação do método
+    public String getNome(){
+        return this.nome;
+    }
+    public int getNumeroDeCreditos(){
+        return this.numeroDeCreditos;
+    }
+    public boolean adicionarAluno(Aluno aluno){
+        if(disciplinaDisponivel()){
+            this.alunosMatriculados.add(aluno);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public void adicionarProfessor(Professor professor){
-        //Implementação do método
+        this.professor = professor;
     }
 
     public void encerrarDisciplina(){
-        //Implementação do método
+        this.ativa = false;
     }
 
-    public List<Aluno> listarAlunosMatriculados(){
-        //Implementação do método
-        return null;
+    public String listarAlunosMatriculados(){
+        String alunosListados = "";
+        for(Aluno aluno: alunosMatriculados){
+           alunosListados += aluno.getNome() + "\n" + aluno.getMatricula() + "\n";
+        }
     }
 
-    public boolean disciplinaAtiva(){
-        //Implementação do método
-        return false;
+    public void inscricaoLiberada(boolean liberarInscricao){
+        this.periodoDeInscricaoLiberado = liberarInscricao;
     }
 
-    public boolean inscricaoLiberada(boolean liberarInscricao){
-        //Implementação do método
-        return false;
-    }
+    private boolean disciplinaDisponivel(){
+        if(this.alunosMatriculados.size() > MIN_DE_ALUNOS && this.alunosMatriculados.size() < MAX_DE_ALUNOS && periodoDeInscricaoLiberado){
+            this.ativa = true;
+            return true;
+        }else{
+            this.ativa = false;
+            return false;
+        }
+    }    
     //#endregion
 }
